eff4f63ae1168fb137c15f5b40bd977df1cd3b77
Akbar KHAN <akhan2023@yahoo.com>
2020-01-02 18:31:59 +0100

    One color works but another need to improve

--- t.js
+++ t.js
@@ -4,7 +4,7 @@ var ctx = canvas.getContext('2d');
 
 // Contains instances of class Fish
 var fishes = [];
-var fishColor = ['red', 'rgb(20, 50, 80)', 'white', 'black', 'skyblue', 'coral', 'yellow', 'lime', 'rgba(0, 0, 200, 0.5)', 'rebeccapurple'];
+var fishColor = ['red', 'rgb(20, 50, 80)', 'white', 'black', 'skyblue', 'coral', 'yellow', 'lime', 'green', 'rebeccapurple'];
 
 class Fish {
   constructor(x, y) {
@@ -12,13 +12,13 @@ class Fish {
     this.y = y;
     this.w = 20;
     this.h = 10;
-    this.color = function() {
-      let i = Math.random()*10;
-      return i = Math.floor(i);
-    };
+    // this.color = function() {
+    //   let i = Math.random()*10;
+    //   return i = Math.floor(i);
+    // }; // todo: give a random color to the fish from array fishColor
     this.id = fishes.length;
-    this.colour = this.color();
-    //ctx.fillStyle = fishColor[this.color()]; // if use this every fish change their color :/       *****COLOR*****
+    // this.colour = this.color();
+    // ctx.fillStyle = fishColor[this.color()];
 
     const fish = this;
     this.move = function () {
@@ -27,7 +27,7 @@ class Fish {
       fish.y += movementY;
       fish.x += movementX;
       // ctx.beginPath();
-      ctx.fillStyle = fishColor[fish.colour]; // It seems like working good but I don't think that is a good idea       *****COLOR*****
+      // ctx.fillStyle = fishColor[fish.colour];
       // debugger;
       ctx.fillRect(fish.x, fish.y, fish.w, fish.h);
     };
@@ -47,9 +47,22 @@ function animateFish() {
 }
 setInterval(animateFish, 10); //todo: adda a narray of 1 and -1 to define a good movement randomly
 
+
+// ctx.fillStyle = 'rgb(200, 0, 0)';
+// ctx.fillRect(10, 10, 50, 50);
+
+// ctx.fillStyle = 'rgba(0, 0, 200, 0.5)';
+// ctx.fillRect(30, 30, 50, 50);
+
+// function draw() {
   function createFish(event) {
     let fish = new Fish(event.offsetX, event.offsetY);
     console.log(fish.x);
   }
 
   document.getElementById('aquarium').addEventListener('click', createFish);
+
+  // requestAnimationFrame(draw);
+// }
+
+// draw();
