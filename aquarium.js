8eb4cf821e6c4f431944cec014b39fa6d997f3ba
Akbar KHAN <akhan2023@yahoo.com>
2020-01-06 18:10:26 +0100

    Code cleaned and one function for each task

--- aquarium.js
+++ aquarium.js
@@ -1,13 +1,54 @@
+var canvas = document.getElementById('aquarium');
+var ctx = canvas.getContext('2d');
+
+// Contains created fishes
+var myFishes = [];
+
+
+function animate() {
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  for (let i = 0; i < myFishes.length; i++) {
+    myFishes[i].move();
+  }
+}
+
+function start(){
+  setInterval(animate, 100);
+}
+
+document.getElementById('button').addEventListener('click', start);
 
 
 
+function getMousePos(canvas, event) {
+  var rect = canvas.getBoundingClientRect();
+  return {
+    x: event.clientX - rect.left,
+    y: event.clientY - rect.top
+  };
+}
+
+/*
+function create(event) {
+  let fish = new Fish(event.offsetX, event.offsetY);
+  return fish;
+}
+*/
+
+function create(event) {
+  let coordinates = getMousePos(canvas, event);
+  console.log(coordinates);
+  let fish = new Fish(coordinates.x, coordinates.y);
+  return fish;
+}
 
 
+function pushFish(newFish) {
+  myFishes.push(newFish);
+}
 
-
-// function aquaLocation(event) {
-//   console.log(event.screenX);
-//   console.log(event.screenY);
-// }
-
-// document.getElementById("aquarium").addEventListener('click', aquaLocation);
+document.getElementById('aquarium').addEventListener('click', (event) => {
+  getMousePos(canvas, event)
+  let createdFish = create(event);
+  pushFish(createdFish);
+});
--- fish/fish.js
+++ fish/fish.js
@@ -1,99 +1,24 @@
-
-var canvas = document.getElementById('aquarium');
-var ctx = canvas.getContext('2d');
-
-// Contains instances of class Fish
-var fishes = [];
-
 function randomNum() {
   let num = Math.floor(Math.random() * 20) + 1;
   num *= Math.floor(Math.random() * 2) == 1 ? 1 : -1;
   return num;
 }
 
-
-/*
-  for(let i =0; randomNum() === -20; i++) {
-    let result = randomNum();
-    console.log(result);
-  }
-*/
-
 class Fish {
+
   constructor(x, y) {
     this.x = x;
     this.y = y;
     this.w = 20;
     this.h = 10;
-    this.id = fishes.length;
-
-    const fish = this;
-    this.move = function () {
-
-      let movementY = randomNum();
-      let movementX = randomNum();
-
-      setInterval()
-
-      if (movementY > 0) {
-        while (movementY !== 0) {
-          setInterval(function () {
-            fish.y += .5;
-            movementY -= .5;
-          }, 10);
-        }
-      }
-      if (movementX > 0) {
-        while (movementX !== 0) {
-          setInterval(function () {
-            fish.x += .5;
-            movementX -= .5;
-          }, 10);
-        }
-      }
-
-      if (movementY < 0) {
-        while (movementY !== 0) {
-          setInterval(function () {
-            fish.y -= .5;
-            movementY += .5;
-          }, 10);
-        }
-      }
-
-      if (movementX < 0) {
-        while (movementX !== 0) {
-          setInterval(function () {
-            fish.x -= .5;
-            movementX += .5;
-          }, 10);
-        }
-      }
-
-
-      // fish.y += randomNum();
-      // fish.x += randomNum();
-      ctx.fillRect(fish.x, fish.y, fish.w, fish.h);
-    };
-
-    // Push each new fish in the fishes array above.
-    fishes.push(this);
-
+    this.id = myFishes.length;
   }
-}
 
-function animateFish() {
-
-  ctx.clearRect(0, 0, canvas.width, canvas.height);
-  for (let i = 0; i < fishes.length; i++) {
-    fishes[i].move();
+  move() {
+    this.y += randomNum();
+    this.x += randomNum();
+    ctx.fillRect(this.x, this.y, this.w, this.h);
   }
-}
-setInterval(animateFish, 100); //todo: adda a narray of 1 and -1 to define a good movement randomly
 
-function createFish(event) {
-  let fish = new Fish(event.offsetX, event.offsetY);
-  console.log(fish.x);
-}
 
-document.getElementById('aquarium').addEventListener('click', createFish);
+}
