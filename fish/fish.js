25f5291cd964578434b94fbc40d1d727e15e8272
Akbar KHAN <akhan2023@yahoo.com>
2020-01-07 17:42:29 +0100

    Fishes move to a random direction

--- fish/fish.js
+++ fish/fish.js
@@ -7,17 +7,38 @@ class Fish {
     this.w = 20;
     this.h = 10;
     this.id = myFishes.length;
+    this.yB = Fish.randomNum();
+    this.xB = Fish.randomNum();
+    this.coefficient = (this.yB - this.y) / (this.xB - this.x);
+    this.inconnue = this.y - this.coefficient * this.x;
+    if (Math.random() > .5) {
+      this.increment = 1
+    } else {
+      this.increment = -1
+    }
   }
 
+
+
   move() {
-    this.y += Fish.randomNum();
-    this.x += Fish.randomNum();
+    
+    this.xCible = this.x + this.increment;
+    this.yCible = this.coefficient * this.xCible + this.inconnue;
+    if(Math.abs(this.yCible - this.y) > Math.abs(this.increment) && Math.abs(this.coefficient) > 0){
+      this.yCible = this.y + this.increment;
+      this.xCible = (this.yCible - this.inconnue) / this.coefficient;
+    }
+    this.x = this.xCible;
+    this.y = this.yCible; 
     ctx.fillRect(this.x, this.y, this.w, this.h);
   }
 
   static randomNum() {
-    let num = Math.floor(Math.random() * 20) + 1;
+    // let num = Math.floor(Math.random() * 500) + 1;
+    // num *= Math.floor(Math.random() * 2) == 1 ? 1 : -1;
+    let num = Math.random() * 10000;
     num *= Math.floor(Math.random() * 2) == 1 ? 1 : -1;
+
     return num;
   }
 
