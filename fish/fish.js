05a6925eb8169a4b23e2c2d1a997f3b19a9d83d5
Akbar KHAN <akhan2023@yahoo.com>
2020-01-10 15:19:35 +0100

    Limit x and y is done, all works

--- fish/fish.js
+++ fish/fish.js
@@ -8,22 +8,22 @@ class Fish {
     this.h = 10;
     this.direction();
     this.id = myFishes.length;
-    Math.random() > .5 ? this.increment = 1 : this.increment = -1;
   }
 
 
   // y=ax et y=ax+b
-  pointSuivant() {
-    this.xTarget = this.x + this.increment;
-    this.yTarget = this.coefficient * this.xTarget + this.inconnue;
-    if (Math.abs(this.yTarget - this.y) > Math.abs(this.increment) && Math.abs(this.coefficient) > 0) {
-      this.yTarget = this.y + this.increment;
-      this.xTarget = (this.yTarget - this.inconnue) / this.coefficient;
+  nextPoint() {
+    this.targetX = this.x + this.increment;
+    this.targetY = this.coefficient * this.targetX + this.inconnue;
+    if (Math.abs(this.targetY - this.y) > Math.abs(this.increment) && Math.abs(this.coefficient) > 0) {
+      this.targetY = this.y + this.increment;
+      this.targetX = (this.targetY - this.inconnue) / this.coefficient;
     }
   }
 
 
   direction() {
+    Math.random() > .5 ? this.increment = 1 : this.increment = -1;
     this.yB = Fish.randomNum();
     this.xB = Fish.randomNum();
     this.coefficient = (this.yB - this.y) / (this.xB - this.x);
@@ -32,12 +32,29 @@ class Fish {
 
   move() {
 
-    this.pointSuivant();
+    this.nextPoint();
+    let limitX = this.targetX + this.w > 800 || this.targetX < 0;
+    let limitY = this.targetY + this.h > 400 || this.targetY < 0;
 
-    if (true) {
-      this.x = this.xTarget;
-      this.y = this.yTarget;
+    // if (!limitX && !limitY) {
+    //   console.log('out');
+    //   this.x = this.targetX;
+    //   this.y = this.targetY;
+    // } else {
+    //   this.direction();
+    // }
+
+    while (limitX || limitY) {
+      this.direction();
+      this.nextPoint();
+      limitX = this.targetX + this.w > 800 || this.targetX < 0;
+      limitY = this.targetY + this.h > 400 || this.targetY < 0;
     }
+  
+    this.x = this.targetX;
+    this.y = this.targetY;
+
+
     ctx.fillRect(this.x, this.y, this.w, this.h);
   }
 
