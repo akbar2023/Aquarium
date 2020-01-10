5b65121c8ebac7124a94ae756b61ce7c03d49c62
Akbar KHAN <akhan2023@yahoo.com>
2020-01-10 12:27:45 +0100

    organizing code to implement aquarium limits

--- fish/fish.js
+++ fish/fish.js
@@ -6,39 +6,44 @@ class Fish {
     this.y = y;
     this.w = 20;
     this.h = 10;
+    this.direction();
     this.id = myFishes.length;
+    Math.random() > .5 ? this.increment = 1 : this.increment = -1;
+  }
+
+
+  // y=ax et y=ax+b
+  pointSuivant() {
+    this.xTarget = this.x + this.increment;
+    this.yTarget = this.coefficient * this.xTarget + this.inconnue;
+    if (Math.abs(this.yTarget - this.y) > Math.abs(this.increment) && Math.abs(this.coefficient) > 0) {
+      this.yTarget = this.y + this.increment;
+      this.xTarget = (this.yTarget - this.inconnue) / this.coefficient;
+    }
+  }
+
+
+  direction() {
     this.yB = Fish.randomNum();
     this.xB = Fish.randomNum();
     this.coefficient = (this.yB - this.y) / (this.xB - this.x);
     this.inconnue = this.y - this.coefficient * this.x;
-    if (Math.random() > .5) {
-      this.increment = 1
-    } else {
-      this.increment = -1
-    }
   }
 
-
-
   move() {
-    
-    this.xCible = this.x + this.increment;
-    this.yCible = this.coefficient * this.xCible + this.inconnue;
-    if(Math.abs(this.yCible - this.y) > Math.abs(this.increment) && Math.abs(this.coefficient) > 0){
-      this.yCible = this.y + this.increment;
-      this.xCible = (this.yCible - this.inconnue) / this.coefficient;
+
+    this.pointSuivant();
+
+    if (true) {
+      this.x = this.xTarget;
+      this.y = this.yTarget;
     }
-    this.x = this.xCible;
-    this.y = this.yCible; 
     ctx.fillRect(this.x, this.y, this.w, this.h);
   }
 
   static randomNum() {
-    // let num = Math.floor(Math.random() * 500) + 1;
-    // num *= Math.floor(Math.random() * 2) == 1 ? 1 : -1;
     let num = Math.random() * 10000;
     num *= Math.floor(Math.random() * 2) == 1 ? 1 : -1;
-
     return num;
   }
 
